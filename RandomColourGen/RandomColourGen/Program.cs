cb31a6b02f218b6b37fc39e829db253f692590e9
awdasdawdsadawdasd <j.lukken@sintlucasedu.nl>
2021-09-30 21:18:06 +0200

    Random colour generator
    Made this for my stepmum, because she needed a random colour pattern.
    
    If you want to use it yourself then the instructions are in the code.

--- RandomColourGen/RandomColourGen/Program.cs
+++ RandomColourGen/RandomColourGen/Program.cs
@@ -7,19 +7,34 @@ namespace RandomColourGen
     {
         static void Main(string[] args)
         {
+            #region VARIABLES BABY!!
             Random rnd = new Random();
+            //Make an extra max*Colour* and set it to 0
+            //For example:
+            //int maxPurple = 0;
             int maxDG = 0;
             int maxG = 0;
             int maxLG = 0;
             int maxGB = 0;
             int maxP = 0;
             int x = 0;
+            #endregion
 
-            List<string> Colours = new List<string> { "Dark Gray", "Gray", "Light Gray", "Green/ Blue", "Pink" };
+            #region WOOO! MAKE THAT LIST, BABY!!!
+            //If you want to add another colour just put it in here ↓                                   for example: ↓
+            List<string> Colours = new List<string> { "Dark Gray", "Gray", "Light Gray", "Green/ Blue", "Pink" /*, "Purple"*/ };
+            #endregion
 
+            #region Loop that bitch
+            //Change this number ↓ to what the max number of your pattern is
             for (int i = 1; i < 36; i++)
             {
-
+                #region Make a random number
+                //put in the number of colours you have so if you have 6 colours:
+                //if (Colours.Count == 6)
+                //{
+                //    x = rnd.Next(0, 6);
+                //} and an else infront of following if
                 if (Colours.Count == 5)
                 {
                     x = rnd.Next(0, 5);
@@ -36,7 +51,15 @@ namespace RandomColourGen
                 {
                     x = rnd.Next(0, 1);
                 }
+                #endregion
 
+                #region Check if colour is used
+                //Add in the max*colour* variable you made at the top
+                //For example:
+                //if (Colours[x] == "Purple")
+                //{
+                //    maxPurple++;
+                //}
                 if (Colours[x] == "Dark Gray")
                 {
                     maxDG++;
@@ -56,11 +79,23 @@ namespace RandomColourGen
                 {
                     maxP++;
                 }
+                #endregion
 
-                    Console.WriteLine($"Position number {i} = {Colours[x]} {maxDG} {maxG} {maxLG} {maxGB} {maxP}");
-                
-                if (maxDG == 7)
-                {
+                #region Write that shit down
+                //You don't need to edit anything here
+                Console.WriteLine($"Position number {i} = {Colours[x]}");
+                #endregion
+
+                #region Check if colour is used at it's max
+                //Add code to check if your extra colours hit their maximum
+                //For example:
+                //if (maxPurple == 7)
+                //{
+                //    Colours.Remove("Purple");
+                //}
+                //Devide the max pattern number by the amount of colours you have
+                if (maxDG == 7/*change this number to the number above ↑ */)
+                {   //NOTE: you can also just put it to random numbers just be sure that the numbers of all the characters adds up to your max pattern colour
                     Colours.Remove("Dark Gray");
                 } if(maxG == 7)
                 {
@@ -75,8 +110,9 @@ namespace RandomColourGen
                 {
                     Colours.Remove("Pink");
                 }
-                
+                #endregion
             }
+            #endregion
         }
     }
 }
